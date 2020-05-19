1d7937cf0229eeb956db009462aa2926eff48612
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-05-19 16:21:50 +0000

    Git Ignore added

--- mongo_project.py
+++ mongo_project.py
@@ -1,7 +1,10 @@
 import pymongo
 import os
+from os import path
+if path.exists("env.py"):
+  import env 
 
-MONGODB_URI = os.getenv("MONGO_URI")
+MONGODB_URI = os.environ.get("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
@@ -13,6 +16,21 @@ def mongo_connect(url):
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No results found.")
+    
+    return doc
 
 def show_menu():
     print("")
@@ -25,14 +43,35 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
+    
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
 
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
-            print("You have selected option 2")
+            get_record()
         elif option == "3":
             print("You have selected option 3")
         elif option == "4":
